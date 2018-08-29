bcdd5ce5ef921037573900084b8b81d6a2b8ca84
paigewill <40868281+paigewill@users.noreply.github.com>
2018-08-29 15:40:33 -0500

    MultiplicationTables

---
+++ MultiplicationTables.java
@@ -0,0 +1,17 @@
+import static java.lang.System.out;
+import java.util.Scanner;
+
+public class MultiplicationTables {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        out.println("Welcome to Multiplication Tables");
+        out.println("");
+        out.println("How large would you like to see them?");
+        int size = keyboard.nextInt();
+        for (int number1 = 0; number1 <= size; number1++) {
+            for (int number2 = 0; number2 <= size; number2++) {
+                out.println(number1 + " * " + number2 + " = " + (number1 * number2));
+            }
+        }
+    }
+}
